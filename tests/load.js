4fe2ac05a7f59446405cf35f2a551374f75a6a8c
alexander-kachur <alexander.kachur@gmail.com>
2017-12-04 21:27:59 +0200

    perf map added

--- tests/load.js
+++ tests/load.js
@@ -9,8 +9,8 @@ export let options = {
         "errors": ["rate<0.01"], // <1% errors
         http_req_duration: ["avg<100", "p(95)<800"]
     },
-    vus: 2,
-    duration: "180s"
+    vus: 5,
+    duration: "60s"
     // stages: [
     //     {duration: "30s", target: 50},
     //     {duration: "2m", vus: 110}
@@ -22,7 +22,7 @@ export default function () {
         calcid: 1234,
         expression: "(2*(9+22/5)-((9-1)/4)^2)+(3^2+((5*5-1)/2))"
     };
-    let res = http.post("http://localhost:8080/api/calc/", payload);
+    let res = http.post("http://192.168.1.3:8080/api/calc/", payload);
     check(res, {
         "status was 200": (r) => r.status == 200,
         // "body contains result": (r) => r.body.indexOf("result") !== -1,
